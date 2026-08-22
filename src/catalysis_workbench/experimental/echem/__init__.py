e6cb2359d0751127e43021a6c093cbf51af248fb
xcwang3333-cloud <xcwang3333@gmail.com>
2026-08-22 20:11:28 +0800

    Export LSV processing API

--- src/catalysis_workbench/experimental/echem/__init__.py
+++ src/catalysis_workbench/experimental/echem/__init__.py
@@ -1,1 +1,23 @@
 """Electrochemical data processing and analysis."""
+
+from .lsv import (
+    LSVError,
+    LSVProcessingConfig,
+    convert_potential_to_rhe,
+    correct_ir_drop,
+    process_lsv,
+    process_lsv_dataset,
+    rhe_offset_from_she,
+    to_current_density,
+)
+
+__all__ = [
+    "LSVError",
+    "LSVProcessingConfig",
+    "convert_potential_to_rhe",
+    "correct_ir_drop",
+    "process_lsv",
+    "process_lsv_dataset",
+    "rhe_offset_from_she",
+    "to_current_density",
+]
