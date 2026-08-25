c33c3c4a0b93ad1bc980ecef62ef4ae8da17b65a
xiaochen wang <xcwang3333@gmail.com>
2026-08-25 15:23:48 +0800

    feat: expose LOBSTER bonding adapters

--- src/catalysis_workbench/io/__init__.py
+++ src/catalysis_workbench/io/__init__.py
@@ -6,6 +6,7 @@ from .electronic_structure import (
     read_chgcar_density,
     read_vasprun_dos,
 )
+from .lobster import LobsterIOError, read_lobster_cohp, read_lobster_icohp
 from .structure import (
     StructureIOError,
     read_cif_structure,
@@ -18,6 +19,7 @@ from .tabular import TabularReadError, read_csv, read_excel, read_tabular, read_
 __all__ = [
     "BaderIOError",
     "ElectronicStructureIOError",
+    "LobsterIOError",
     "StructureIOError",
     "TabularReadError",
     "read_bader_acf",
@@ -26,6 +28,8 @@ __all__ = [
     "read_contcar",
     "read_csv",
     "read_excel",
+    "read_lobster_cohp",
+    "read_lobster_icohp",
     "read_poscar",
     "read_tabular",
     "read_txt",
