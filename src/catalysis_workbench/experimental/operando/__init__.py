09523db347686a19a34dbe778633de23e43de046
xiaochen wang <xcwang3333@gmail.com>
2026-08-26 10:10:47 +0800

    feat: export v0.8 Block 1 operando API (#253)

--- src/catalysis_workbench/experimental/operando/__init__.py
+++ src/catalysis_workbench/experimental/operando/__init__.py
@@ -1,1 +1,17 @@
 """Shared immutable operando/time-resolved data foundation."""
+
+from .stack import (
+    FrameCoordinate,
+    OperandoStack,
+    OperandoStackError,
+    build_operando_stack,
+    series_array_digest,
+)
+
+__all__ = [
+    "FrameCoordinate",
+    "OperandoStack",
+    "OperandoStackError",
+    "build_operando_stack",
+    "series_array_digest",
+]
